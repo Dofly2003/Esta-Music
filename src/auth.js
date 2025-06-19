b81297491175390121dbd3f3616ce8bc1163eda4
Dony <doniananda622@gmail.com>
2025-06-19 10:22:37 +0700

    comit ke 2

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
 const clientId = "a0d0b65251a04e6aa5230da17b2405b6"; // ganti dengan milikmu
-const redirectUri = "https://esta.music.com";
+const redirectUri = "https://esta-music.vercel.app/";
 const scopes = [
   "user-read-private",
   "user-read-email",
